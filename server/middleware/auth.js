0374f6fbcf20eecc973b27e649a7df2e5bf1586a
Tony <xaj@gmail.com>
2021-06-18 15:32:36 +0800

    fix au

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,9 +5,9 @@ module.exports = options => {
 
     return async (req,res,next) => {
         const token = String(req.headers.authorization || '').split(' ').pop()
-        assert(token,401,'请提供 jwt token')
+        assert(token,401,'请先登录')
         const {id} = jwt.verify(token,req.app.get('secret'))
-        assert(id,401,'无效的 jwt token')
+        assert(id,401,'请先登录')
         req.user = await AdminUser.findById(id)
         assert(req.user,401,'请先登录')
         await next()
